c77ae133834dc2c4b5d3c12bca36ddf609b76f9d
Gor Oleg <GorOleg@yandex.ru>
2023-06-27 23:33:04 +0300

    Answer 2023-06-24 1
    Signed-off-by: Gor Oleg <GorOleg@yandex.ru>

--- 3.3/L_33_G.py
+++ 3.3/L_33_G.py
@@ -1,1 +1,2 @@
-# 1
+numbers = {1, 2, 3, 4, 5}
+print({number: [i for i in range(1, number + 1) if not number % i] for number in numbers})
---
+++ 3.3/L_33_H.py
@@ -0,0 +1,2 @@
+string = 'Российская Федерация'
+print(''.join(word[0] for word in string.split()).upper())
