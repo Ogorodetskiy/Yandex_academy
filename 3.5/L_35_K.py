fc12ab63a1d0513ead103308c378375422346311
Gor Oleg <GorOleg@yandex.ru>
2024-06-08 11:25:52 +0300

    add 3.5.K

--- 3.5/L_35_K.py
+++ 3.5/L_35_K.py
@@ -1,18 +1,19 @@
 import json
 
-file = 'numbers.txt'
-# input()
+file_i = input()
+file_o = input()
+
 txt = []
-with open(file, encoding="UTF-8") as file_in:
+
+with open(file_i, encoding="UTF-8") as file_in:
     for line in file_in:
         txt += map(int, line.split())
 
-print('{',
-    '"count:"' + str(len(txt)),
-    len([i for i in txt if i > 0]),
-    min(txt) if len(txt) != 0 else 0,
-    max(txt) if len(txt) != 0 else 0,
-    sum(txt),
-    round(sum(txt) / len(txt), 2) if len(txt) != 0 else 0,
-    sep="\n",
-)
+res = {'count': len(txt), 'positive_count': len([i for i in txt if i > 0]),
+       'min': min(txt) if len(txt) != 0 else 0,
+       'max': max(txt) if len(txt) != 0 else 0,
+       'sum': sum(txt),
+       'average': round(sum(txt) / len(txt), 2) if len(txt) != 0 else 0}
+
+with open(file_o, "w", encoding="UTF-8") as file_out:
+    json.dump(res, file_out, ensure_ascii=False, indent=4)
---
+++ 3.5/L_35_L.py
@@ -0,0 +1,6 @@
+numbers = 'numbers.txt'
+even = 'even.txt'
+odd = 'odd.txt'
+eq = 'eq.txt'
+
+# with open("file1.txt") as f1, open("file2.txt") as f2:
