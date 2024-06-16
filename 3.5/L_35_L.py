025bf6291c13c3a107c6fca0197c6d3c5465d921
Gor Oleg <GorOleg@yandex.ru>
2024-06-16 23:12:20 +0300

    add 3.5.M

--- 3.5/L_35_L.py
+++ 3.5/L_35_L.py
@@ -1,6 +1,34 @@
-numbers = 'numbers.txt'
-even = 'even.txt'
-odd = 'odd.txt'
-eq = 'eq.txt'
+numbers = input()
+even = input()
+odd = input()
+eq = input()
 
-# with open("file1.txt") as f1, open("file2.txt") as f2:
+with (
+    open(numbers, "r", encoding="UTF-8") as file_in,
+    open(even, "w", encoding="UTF-8") as f_out_even,
+    open(odd, "w", encoding="UTF-8") as f_out_odd,
+    open(eq, "w", encoding="UTF-8") as f_out_eq,
+):
+
+    for line in file_in.readlines():
+        v_even = []
+        v_odd = []
+        v_eq = []
+
+        for n in line.split():
+            q = 0
+
+            for x in n:
+                if int(x) % 2 == 0:
+                    q += 1
+
+            if q > len(n) - q:
+                v_even.append(n)
+            elif q < len(n) - q:
+                v_odd.append(n)
+            else:
+                v_eq.append(n)
+
+        print(*v_even, file=f_out_even)
+        print(*v_odd, file=f_out_odd)
+        print(*v_eq, file=f_out_eq)
